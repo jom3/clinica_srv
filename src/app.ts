49ac2b5edba7efc604ec7db7c26102baafe4d293
joams mogro gomez <joamsmg@gmail.com>
2023-10-26 00:19:21 -0400

    some changes added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import "reflect-metadata"
 import express from 'express'
 import cors from 'cors'
+import { connection } from './config/db.config'
+import { appRouter } from './routes/app.routes'
 
 const app = express()
 const port = process.env.PORT ?? 3000
@@ -10,8 +12,8 @@ app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.get('', (req, res)=>{
-  res.send('hola mundo')
-})
+connection()
+
+app.use(appRouter)
 
 app.listen(port, ()=> console.log(`Server listening on port ${port}`))
