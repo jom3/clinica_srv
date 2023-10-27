232fabd2d2e070cd83674cce88d54ef5c7279c50
joams mogro gomez <joamsmg@gmail.com>
2023-10-26 23:21:35 -0400

    auth routes added into main routes

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRoutes } from "./user.routes";
+import { authRoutes } from "./auth.routes";
 
 const appRouter = Router()
 
 appRouter.use('/user', userRoutes)
+appRouter.use('/auth', authRoutes)
 
 export { appRouter }
