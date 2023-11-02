37153aae0832df479c87e9e9b084641ab269c537
joams mogro gomez <joamsmg@gmail.com>
2023-11-02 19:31:05 -0400

    department routes added into main route

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { userRoutes } from "./user.routes";
 import { authRoutes } from "./auth.routes";
+import { departmentRoutes } from "./department.routes";
 
 const appRouter = Router()
 
 appRouter.use('/user', userRoutes)
 appRouter.use('/auth', authRoutes)
+appRouter.use('/department', departmentRoutes)
 
 export { appRouter }
