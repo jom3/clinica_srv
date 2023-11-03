f7470483b06c110a4cb97bf2a6f964b32cf19977
joams mogro gomez <joamsmg@gmail.com>
2023-11-02 21:38:25 -0400

    speciality routes added into main router

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { userRoutes } from "./user.routes";
 import { authRoutes } from "./auth.routes";
 import { departmentRoutes } from "./department.routes";
+import { specialityRoutes } from "./speciality.routes";
 
 const appRouter = Router()
 
 appRouter.use('/user', userRoutes)
 appRouter.use('/auth', authRoutes)
 appRouter.use('/department', departmentRoutes)
+appRouter.use('/speciality', specialityRoutes)
 
 export { appRouter }
