c1b986dc46ec964d8da1575eeab1c92a7e609a26
joams mogro gomez <joamsmg@gmail.com>
2023-11-03 13:09:22 -0400

    staff routes added into main router

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -3,6 +3,7 @@ import { userRoutes } from "./user.routes";
 import { authRoutes } from "./auth.routes";
 import { departmentRoutes } from "./department.routes";
 import { specialityRoutes } from "./speciality.routes";
+import { staffRoutes } from "./staff.routes";
 
 const appRouter = Router()
 
@@ -10,5 +11,6 @@ appRouter.use('/user', userRoutes)
 appRouter.use('/auth', authRoutes)
 appRouter.use('/department', departmentRoutes)
 appRouter.use('/speciality', specialityRoutes)
+appRouter.use('/staff', staffRoutes)
 
 export { appRouter }
