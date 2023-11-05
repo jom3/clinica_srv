4f0ead76daefc4df585333963be7129733ff6796
joams mogro gomez <joamsmg@gmail.com>
2023-11-04 21:08:48 -0400

    illness routes added into main router

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -4,12 +4,14 @@ import { authRoutes } from "./auth.routes";
 import { departmentRoutes } from "./department.routes";
 import { specialityRoutes } from "./speciality.routes";
 import { staffRoutes } from "./staff.routes";
+import { illnessRoutes } from "./illness.routes";
 
 const appRouter = Router()
 
 appRouter.use('/user', userRoutes)
 appRouter.use('/auth', authRoutes)
 appRouter.use('/department', departmentRoutes)
+appRouter.use('/illness', illnessRoutes)
 appRouter.use('/speciality', specialityRoutes)
 appRouter.use('/staff', staffRoutes)
 
